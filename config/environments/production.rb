24f849cb08b1ef225f22dc918d2229a954bff506
radha017 <radharanisantos.pr@gmail.com>
2023-09-07 17:13:30 +0800

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,8 +95,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "radhasantos59@gmail.com",
-    :password             => "ghbdmqqfdhnphcav",
+    :user_name            => ENV[GMAIL_USERNAME],
+    :password             => ENV[GMAIL_PASSWORD],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
