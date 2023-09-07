e11501ee3f04132fd12ff4c80b752e6ff68c30ed
radha017 <radharanisantos.pr@gmail.com>
2023-09-07 16:49:38 +0800

    return  the send upon registration and fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",,
+    :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => ENV['GMAIL_USERNAME'],
     :password             => ENV['GMAIL_PASSWORD'],
